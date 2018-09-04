53559fc306bf0069ebbd1ef39ecb96652c695540
Mahelder Melo <mahelder.melo@luizalabs.com>
2018-09-04 13:46:01 -0300

    handler logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 import logging
-from vibora import Vibora
-
+from vibora import Vibora, Request
+from vibora.responses import Response
+from vibora.hooks import Events
 from app.api import api
 from app.user.views import user_api
 from app.user.model import db
-from app.hooks.logging import teste
 
 from raven.conf import setup_logging
 from raven.handlers.logging import SentryHandler
@@ -14,8 +14,6 @@ app = Vibora()
 app.add_blueprint(user_api, prefixes={'v1': '/v1'})
 app.add_blueprint(api, prefixes={'v1': '/v1'})
 
-app.add_hook(teste)
-
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
 
@@ -24,3 +22,17 @@ handler = SentryHandler(
 handler.setLevel(logging.ERROR)
 
 setup_logging(handler)
+
+logger = logging.getLogger(__name__)
+
+
+@app.handle(Events.BEFORE_ENDPOINT)
+async def before_response(request: Request):
+    print("passei aqui")
+    logger.info("passei aqui")
+
+
+@app.handle(Events.AFTER_ENDPOINT)
+async def after_response(response: Response):
+    print("passei aqui 2")
+    logger.info("passei aqui 2")
